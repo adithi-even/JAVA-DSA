63160cb9760e3e438ccc474da4d5bc45e8351693
adithi-even <adithigopal1409@gmail.com>
2025-03-21 18:03:31 +0530

    learnet how to take input from the user and print it by using many keywords

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World@@");
+        System.out.println("Adithi");
+        System.out.println(12345678);
+        System.out.println("hello world");
+        Scanner input = new Scanner(System.in);  //input variable with the type Scanner
+        System.out.println(input.nextLine());  //input.nextInt() is used to take integer input
+
+
     }
+    
 }
+
+//inputs in java
